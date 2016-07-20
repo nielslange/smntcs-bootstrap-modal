2ce96754aeee00535c24160338e69323f7f29cf6
Niels Lange <info@nielslange.de>
2016-07-20 15:40:17 +0800

    Initial push

---
+++ readme.txt
@@ -0,0 +1,45 @@
+ === SMNTCS Bootstrap Modal ===
+Contributors: nielslange
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=LGS558RFNR3JY
+Tags: Twitter Bootstrap Modal, modal, modal window, modal form, modal box, popup, popup box, popup form, popup window  
+Requires at least: 4.0
+Tested up to: 4.5.3
+Stable tag: 1.3.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Open any shortcode within a <a href="http://getbootstrap.com/javascript/#modals" target="_blank">Twitter Bootstrap</a> modal
+
+== Description ==
+
+Tired of looking for "another" plugin to open your favorite contact form as a popup? Don't look any further. SMNTCS Bootstrap Modal allows you to open any shortcode, e.g. from Contact Form 7 or Gravity Forms, within a Twitter Bootstrap Modal. 
+
+== Installation ==
+
+1. Upload 'smntcs-bootstrap-modal' to the '/wp-content/plugins/' directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Create a form, e.g. using Contact Form 7 (https://wordpress.org/plugins/contact-form-7/)
+4. Grab the shortcode and go to the settings page of the plugin
+5. Provide a title, a button text and a shortcode hit the “Save Changes” button
+6. Copy the provided shortcode and paste it within a post, page or widget
+
+== Screenshots ==
+
+1. Upload and activate the plugin
+2. Provide a title, a button text and a shortcode hit the “Save Changes” button
+3. Copy the provided shortcode and paste it within a post, page or widget
+4. See the plugin in action
+
+== Changelog ==
+
+= 1.3.0 =
+* Add donation link
+
+= 1.2.0 =
+* Update domain path
+
+= 1.1.0 =
+* Update textdomain
+
+= 1.0.0 =
+* Initial release
---
+++ smntcs-bootstrap-modal.php
@@ -0,0 +1,137 @@
+<?php
+/*
+Plugin Name: SMNTCS Bootstrap Modal
+Description: Open any shortcode within a <a href="http://getbootstrap.com/javascript/#modals" target="_blank">Twitter Bootstrap</a> modal.
+Author: Niels Lange
+Author URI: http://www.nielslange.de
+Text Domain: smntcs-bootstrap-modal
+Domain Path: /languages/plugins/smntcs-bootstrap-modal
+Version: 1.3.0 
+*/
+
+/*  Copyright 2014-2016	Niels Lange (email : info@nielslange.de)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+// Activate plugin
+register_activation_hook(__FILE__, 'smntcs_bootstrap_modal_activate_plugin');
+function smntcs_bootstrap_modal_activate_plugin() {
+	add_option('smntcs_bootstrap_modal_title', '');
+	add_option('smntcs_bootstrap_modal_shortcode', '');
+}
+
+// Deactivate plugin
+register_deactivation_hook(__FILE__, 'smntcs_bootstrap_modal_deactivate_plugin');
+function smntcs_bootstrap_modal_deactivate_plugin() {
+	delete_option('smntcs_bootstrap_modal_title');
+	delete_option('smntcs_bootstrap_modal_button');
+	delete_option('smntcs_bootstrap_modal_shortcode');
+}
+
+// Initialize plugin
+function smntcs_bootstrap_modal_admin_init() {
+	register_setting('smntcs_bootstrap_modal', 'smntcs_bootstrap_modal_title');
+	register_setting('smntcs_bootstrap_modal', 'smntcs_bootstrap_modal_button');
+	register_setting('smntcs_bootstrap_modal', 'smntcs_bootstrap_modal_shortcode');
+}
+
+// Add menu item in backend
+function smntcs_bootstrap_modal_admin_menu() {
+	add_options_page('Bootstrap Modal', 'Bootstrap Modal', 'manage_options', 'bootstrap-modal', 'smntcs_bootstrap_modal_options_page');
+}
+
+// Add options page in backend
+function smntcs_bootstrap_modal_options_page() {
+	include(WP_PLUGIN_DIR . '/smntcs-bootstrap-modal/options.php');
+}
+
+// Initialize show plugin in backend
+if (is_admin()) {
+	add_action('admin_init', 'smntcs_bootstrap_modal_admin_init');
+	add_action('admin_menu', 'smntcs_bootstrap_modal_admin_menu');
+}
+
+// Load translation(s)
+add_action('plugins_loaded', 'smntcs_bootstrap_modal_load_textdomain');
+function smntcs_bootstrap_modal_load_textdomain() {
+	load_plugin_textdomain( 'smntcs-bootstrap-modal', false, false, plugin_basename( dirname( __FILE__ ) ) . '/languages' );
+}
+
+// Add settings link on plugin page
+$plugin = plugin_basename(__FILE__);
+add_filter("plugin_action_links_$plugin", 'smntcs_bootstrap_modal_plugin_settings_link' );
+function smntcs_bootstrap_modal_plugin_settings_link($links) {
+	$settings_link = '<a href="options-general.php?page=bootstrap-modal">' . __('Settings', 'smntcs-bootstrap-modal') . '</a>';
+	array_unshift($links, $settings_link);
+	return $links;
+}
+
+// Define shortcode
+add_shortcode('smntcs-modal', 'smntcs_bootstrap_modal_define_shortcode');
+function smntcs_bootstrap_modal_define_shortcode() {
+	?>
+    <p><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal"><?php echo get_option('smntcs_bootstrap_modal_button'); ?></button></p>
+    <?php
+}
+
+// Apply custom CSS to frontend
+add_action('wp_head', 'smntcs_bootstrap_modal_css_frontend');
+function smntcs_bootstrap_modal_css_frontend() {
+	?>
+<style type="text/css" media="screen">
+	.modal-open { overflow: visible; }
+	.modal-open, .modal-open .navbar-fixed-top, .modal-open .navbar-fixed-bottom { padding-right:0px!important; }
+</style>
+    <?php
+}
+
+// Apply custom CSS to frontend
+add_action('admin_head', 'smntcs_bootstrap_modal_css_backend');
+function smntcs_bootstrap_modal_css_backend() {
+	?>
+<style type="text/css" media="screen">
+    span.shortcode { display: block; margin: 2px 0;}
+    span.shortcode > input { background: inherit; color: inherit; font-size: 12px; border: none; box-shadow: none; padding: 4px 8px; margin: 0; }
+</style>
+    <?php
+}
+
+// Show site verification code in frontend
+add_action('wp_footer', 'smntcs_bootstrap_modal');
+function smntcs_bootstrap_modal() {
+	wp_register_style( 'bootstrap-css', plugins_url('assets/bootstrap-3.3.6/css/bootstrap.min.css', __FILE__), '' , '3.3.6', 'all' );
+	wp_enqueue_style( 'bootstrap-css' );
+	wp_register_script( 'bootstrap-js', plugins_url('assets/bootstrap-3.3.6/js/bootstrap.min.js', __FILE__), array( 'jquery' ), '3.3.6', true );
+	wp_enqueue_script( 'bootstrap-js' );
+	?>
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="myModalLabel"><?php echo get_option('smntcs_bootstrap_modal_title'); ?></h4>
+                </div>
+                <div class="modal-body">
+                    <?php echo do_shortcode(get_option('smntcs_bootstrap_modal_shortcode')); ?>
+                </div>
+                <div class="modal-footer">
+			        <button type="button" class="btn btn-default" data-dismiss="modal"><?php _e('Close', 'smntcs-bootstrap-modal'); ?></button>
+	           </div>
+            </div>
+        </div>
+    </div>
+    <?php
+}

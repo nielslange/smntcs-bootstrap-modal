33eb06520d80ec01080f986213322d9a010d3963
Niels Lange <info@nielslange.de>
2019-04-05 13:41:39 +0700

    Refactored based on PHPCS and WPCS

--- options.php
+++ options.php
@@ -1,41 +1,55 @@
+<?php
+/**
+ * The options page of this plugin.
+ *
+ * @category   Plugin
+ * @package    WordPress
+ * @subpackage SMNTCS Bootstrap Modal
+ * @author     Niels Lange <info@nielslange.de>
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+
+?>
 <div class="wrap">
 
-    <h2>Bootstrap Modal</h2>
+	<h2>Bootstrap Modal</h2>
 
-    <form method="post" action="options.php">
+	<form method="post" action="options.php">
 
-        <?php wp_nonce_field('update-options'); ?>
-        <?php settings_fields('smntcs_bootstrap_modal'); ?>
+		<?php wp_nonce_field( 'update-options' ); ?>
+		<?php settings_fields( 'smntcs_bootstrap_modal' ); ?>
 
-        <?php if (get_option('smntcs_bootstrap_modal_title') && get_option('smntcs_bootstrap_modal_shortcode')) : ?>
-        <div class="inside">
-	        <p class="description">
-	            <label for="smntcsbm-shortcode">Copy this shortcode and paste it into your post, page, or text widget content:</label>
-	            <span class="shortcode wp-ui-highlight"><input type="text" id="smntcsbm-shortcode" onfocus="this.select();" readonly="readonly" class="large-text code" value="[smntcs-modal]"></span>
-	        </p>
-        </div>
-        <?php endif; ?>
+		<?php if ( get_option( 'smntcs_bootstrap_modal_title' ) && get_option( 'smntcs_bootstrap_modal_shortcode' ) ) : ?>
 
-        <table class="form-table">
-            <tr valign="top">
-                <th scope="row"><?php _e('Title:', 'smntcs-bootstrap-modal') ?></th>
-                <td><input type="text" name="smntcs_bootstrap_modal_title" value="<?php echo get_option('smntcs_bootstrap_modal_title'); ?>" size="50" /></td>
-            </tr>
-            <tr valign="top">
-                <th scope="row"><?php _e('Button:', 'smntcs-bootstrap-modal') ?></th>
-                <td><input type="text" name="smntcs_bootstrap_modal_button" value="<?php echo get_option('smntcs_bootstrap_modal_button'); ?>" size="50" /></td>
-            </tr>
-            <tr valign="top">
-                <th scope="row"><?php _e('Shortcode:', 'smntcs-bootstrap-modal') ?></th>
-                <td><input type="text" name="smntcs_bootstrap_modal_shortcode" value="<?php echo esc_textarea(get_option('smntcs_bootstrap_modal_shortcode')); ?>" size="50" /></td>
-            </tr>
-        </table>
+		<div class="inside">
+			<p class="description">
+				<label for="smntcsbm-shortcode">Copy this shortcode and paste it into your post, page, or text widget content:</label>
+				<span class="shortcode wp-ui-highlight"><input type="text" id="smntcsbm-shortcode" onfocus="this.select();" readonly="readonly" class="large-text code" value="[smntcs-modal]"></span>
+			</p>
+		</div>
 
-        <p class="submit">
-            <input type="hidden" name="action" value="update" />
-            <input type="submit" class="button-primary" value="<?php _e('Save Changes', 'smntcs-bootstrap-modal'); ?>" />
-        </p>
+		<?php endif; ?>
 
-    </form>
+		<table class="form-table">
+			<tr valign="top">
+				<th scope="row"><?php esc_attr_e( 'Title:', 'smntcs-bootstrap-modal' ); ?></th>
+				<td><input type="text" name="smntcs_bootstrap_modal_title" value="<?php echo esc_attr( get_option( 'smntcs_bootstrap_modal_title' ) ); ?>" size="50" /></td>
+			</tr>
+			<tr valign="top">
+				<th scope="row"><?php esc_attr_e( 'Button:', 'smntcs-bootstrap-modal' ); ?></th>
+				<td><input type="text" name="smntcs_bootstrap_modal_button" value="<?php echo esc_attr( get_option( 'smntcs_bootstrap_modal_button' ) ); ?>" size="50" /></td>
+			</tr>
+			<tr valign="top">
+				<th scope="row"><?php esc_attr_e( 'Shortcode:', 'smntcs-bootstrap-modal' ); ?></th>
+				<td><input type="text" name="smntcs_bootstrap_modal_shortcode" value="<?php echo esc_textarea( get_option( 'smntcs_bootstrap_modal_shortcode' ) ); ?>" size="50" /></td>
+			</tr>
+		</table>
+
+		<p class="submit">
+			<input type="hidden" name="action" value="update" />
+			<input type="submit" class="button-primary" value="<?php esc_attr_e( 'Save Changes', 'smntcs-bootstrap-modal' ); ?>" />
+		</p>
+
+	</form>
 
 </div>
